b7c7b261d2bc989ea66ba6d4b61bcaa7239cdc03
Nims09 <nmots@live.ca>
2015-09-01 21:50:32 -0700

    pulled search function out somewhat, got working on time limits with entities

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -18,8 +18,42 @@ class Tag < ActiveRecord::Base
 	# 	=> Need to think about how this is done intially
 	# 	=> Perhaps come to some specified winow, or how do we know how many initial tweets we need, initial analysis 	
 	def intiate_sentiment
-		client = TwitterAPI.new.client
 
+		search = get_tweets_in_time_range( "#justin", (Time.now-1.weeks), Time.now)
+
+		search.each do |tweet|
+			puts tweet.text
+		end
+
+		puts search.count
 
 	end
+
+	private
+
+	# TODO : =>	This needs to be replaced by web scraping. 
+	# 			The API simply does not cover the needs for 
+	# 			an application like this with the strict 
+	# 			limits. We will use the gem for now. 
+	# 		 => This limit appears to be with the NUMBER of
+	# 			requests you, not the actual amount of tweets
+	# 			being returned in each
+	def get_tweets_in_time_range(search, since_time, until_time)
+		client = TwitterAPI.new.client
+
+		begin
+
+			return client.search(
+				search,
+				include_entities: true,
+				result_type: "recent",
+				since: since_time.to_date, 
+				until: until_time.to_date
+			)
+		rescue Twitter::Error::Unauthorized
+			puts "Unauthorized credentials"
+
+			return []
+		end
+	end 
 end
